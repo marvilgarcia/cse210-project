543ccee3797c751f760891eb3ec36acb72dc2875
marvil garcia <marvigarcia015@gmail.com>
2024-01-13 17:33:44 -0400

    Prep2 changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
-class Program
+
+Console.Write("What is your percentage score? ");
+string score = Console.ReadLine();
+
+int x =  int.Parse(score);
+
+if (x >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    Console.WriteLine("Your letter grade is A");
+}
+else if (x < 90 && x >= 80)
+{
+    Console.WriteLine("Your letter grade is B");
+
+}
+else if (x < 80 && x >= 70)
+{
+    Console.WriteLine("Your letter grade is C");
+}
+else if(x < 70 && x >= 60)
+{
+    Console.WriteLine("Your letter grade is D");
+    
+}
+else 
+{
+    Console.WriteLine("Your letter grade is F");
 }
