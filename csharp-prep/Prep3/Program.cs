b7a2d2cda25ab72099314cfb2c70a87eee8fafa9
marvil garcia <marvigarcia015@gmail.com>
2024-01-14 01:36:10 -0400

    Assigments changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int x = int.Parse(magicNumber);
+
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < x)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > x)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break; // Exit the loop when the guess is correct
+            }
+        }
+
+        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+            {
+                break; 
+            }
+
+            numbers.Add(input);
+        }
+
+        if (numbers.Count > 0)
+        {
+            // Compute the sum
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            // Compute the average
+            double average = (double)sum / numbers.Count;
+
+            // Find the maximum number
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered.");
+        }
     }
 }
