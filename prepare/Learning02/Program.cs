f38118b32d467e41e00f39d62a659682dde1e002
marvil garcia <marvigarcia015@gmail.com>
2024-01-31 11:37:44 -0400

    Learning02 activity changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Sotware Enginner";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Junior developer";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+
+        Resume resume1 = new Resume();
+        resume1._name =  "Marvil Garcia";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
+
+
+
+
+
+
     }
 }
